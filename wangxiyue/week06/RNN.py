a7437605fffc9fd617c3342c344586e35695b705
虫子 <wangxiyue.xy@163.com>
2025-04-07 10:44:33 +0800

    1 week6 调整代码结构

--- wangxiyue/week06/RNN.py
+++ wangxiyue/week06/RNN.py
@@ -14,12 +14,10 @@ import torch
 from sklearn.datasets import fetch_olivetti_faces
 from sklearn.model_selection import train_test_split
 from torch import nn
-from torch.nn.functional import max_pool1d
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
-
-from wangxiyue.week03.torch_nn_full import optimizer
+from torchvision.datasets import MNIST
 
 
 def check_device():
@@ -41,28 +39,23 @@ def load_data_olivettiface():
     # data,img,target,desc = fetch_olivetti_faces(data_home='../data/face_data',return_X_y=False)
     data_all =  fetch_olivetti_faces(data_home='../data/face_data', return_X_y=False)
 
-    data = data_all["data"]
     img = data_all["images"]
     target = data_all["target"]
-    return train_test_split(data,target,img,test_size=0.2,random_state=42,shuffle=True)
+    # 归一化处理
+    mean = img.mean()
+    std = img.std()
+    img = (img - mean) / std
+    return train_test_split(img,target,test_size=0.2,random_state=42,shuffle=True)
 
 # torch DataLoader
 def data_loader(batch_size,device):
-    transform_train = transforms.Compose([
-        transforms.RandomRotation(16),
-        transforms.RandomAffine(7, translate=(0.11, 0.13), shear=0.16),
-        transforms.ToTensor()
-    ])
-    ######
-    transform_train = None
-    ######
 
-    xTrain, xTest, yTrain, yTest,imgTrain,imgTest = load_data_olivettiface()
+    xTrain, xTest, yTrain, yTest = load_data_olivettiface()
     labels = set(yTrain)
     print('people label = ', len(labels))
-    # print('load Img ==> ',imgTrain.shape,imgTest.shape)
-    train_datasets = OlivettiDataset(xTrain,yTrain , transform=transform_train)
-    test_datasets = OlivettiDataset(xTest, yTest , img = imgTest,toTensor=False)
+
+    train_datasets = OlivettiDataset(xTrain,yTrain )
+    test_datasets = OlivettiDataset(xTest, yTest)
 
     trainData = DataLoader(train_datasets, batch_size=batch_size, shuffle=True
                             ,generator=torch.Generator(device=device))
@@ -72,18 +65,12 @@ def data_loader(batch_size,device):
 
 #torch Dataset
 class OlivettiDataset(Dataset):
-    def __init__(self,x,y,img = None,transform=None , toTensor=True):
-        # super(OlivettiDataset,self).__init__()
+    def __init__(self,x,y, toTensor=transforms.ToTensor()):
+        super(OlivettiDataset,self).__init__()
         self.x = x
         self.y = y
         self.toTensor = toTensor
-        self.img = img
-        if self.toTensor==False:
-            print('self.toTensor==False  x.shape ==> ',x.shape)
-        if transform is not None:
-            self.transform = transform
-        else :
-            self.transform = None
+
 
     def __len__(self):
         return len(self.x)
@@ -91,20 +78,8 @@ class OlivettiDataset(Dataset):
     def __getitem__(self,idx):
         data = self.x[idx]
         target = self.y[idx]
-        img = None
-        if self.img is not None:
-            img = self.img[idx]
-
-        if self.transform is not None:
-            data = self.transform(data[idx])
-            target=self.transform(target[idx])
-        if self.toTensor:
-            data =torch.tensor(data)
-            target = torch.tensor(target)
-            # if self.img is not None:
-            #     img = torch.tensor(img)
-        if self.img is not None:
-            return data, target, img
+        data =torch.tensor(data)
+        target = torch.tensor(target)
         return data, target
 
 class color:
@@ -119,11 +94,24 @@ class color:
     UNDERLINE = '\033[4m'
     END = '\033[0m'
 
+def data_loader_mnist(batch_size,device):
+    train = MNIST(root='../data/MNIST', train=True, download=True, transform=transforms.ToTensor())
+    test = MNIST(root='../data/MNIST', train=False, download=True, transform=transforms.ToTensor())
+
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True ,generator=torch.Generator(device=device))
+    test_loader = DataLoader(test, batch_size=batch_size, shuffle=False ,generator=torch.Generator(device=device))
+    return train_loader,test_loader
 
 class CustomTorchRNN(nn.Module):
     def __init__(self, input_size, hidden_size,num_layers, output_size):
         super(CustomTorchRNN, self).__init__()
-        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_size,num_layers=num_layers, batch_first=True)
+        self.rnn = nn.RNN(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            bias=True,
+            batch_first=True
+        )
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
@@ -134,40 +122,122 @@ class CustomTorchRNN(nn.Module):
         y = self.fc(end[-1,:,:])
         return y
 
-if __name__ == '__main__':
-
-    # check device
-    device = check_device()
-    torch.set_default_device(device)
 
 
-    ############################# 超参 #################################
-    Learning_Rate = 0.005
-    Epochs = 10
-    BATCH_SIZE = 64
-    Momentum = 0.9
-    #
-    input_size = 64
-    hidden_size = 50
-    num_layers = 5
-    output_size = 40 # 40个人
-    model = CustomTorchRNN(input_size=input_size, hidden_size=hidden_size,num_layers=num_layers, output_size=output_size)
-    print('model initialized')
-    Optimizer = torch.optim.AdamW(model.parameters(),lr=Learning_Rate)
-    ############################# 配置 #################################
-    LOG_DIR = '../data/logs_olivetti'
-    if os.path.exists(LOG_DIR): shutil.rmtree(LOG_DIR)
-    writer = SummaryWriter(LOG_DIR)
-    start_time = time.time()
+###################################################################
+###
+###   public param
+###
+###################################################################
+# check device
+device = check_device()
+torch.set_default_device(device)
+############################# 超参 #################################
+Learning_Rate = 0.005
+Epochs = 10
+BATCH_SIZE = 5
+Momentum = 0.9
+#
+
+
+############################# 配置 #################################
+LOG_DIR = '../data/logs_rnn'
+if os.path.exists(LOG_DIR): shutil.rmtree(LOG_DIR)
+writer = SummaryWriter(LOG_DIR)
+start_time = time.time()
+
+############################# 加载数据 ########################
+#   b t c h w
+#  weight = 64 ， height = 64
+
+def train_mnist():
     ############################# 训练数据参数 #########################
+    input_size = 28  # face: h = 64 , mnist: h= 28
+    hidden_size = 50
+    num_layers = 1
+    output_size = 10  # face: 40个人 , mnist : 10 个数字
+
     total_train_step = 0  # 总体训练集次数
     total_test_step = 0  # 总体验证集次数
-    ############################# 加载数据 ########################
-    #   b t c h w
-    #  weight = 64 ， height = 64
+
+    ##
+    model = CustomTorchRNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers,
+                           output_size=output_size)
+    print('model initialized')
+    Optimizer = torch.optim.SGD(model.parameters(), lr=Learning_Rate, momentum=Momentum)
+
+    train_data, test_data = data_loader_mnist(BATCH_SIZE, device)
+
+    loss_fc = nn.CrossEntropyLoss()
+    start_time = time.time()
+    for epoch in range(Epochs):
+        model.train()
+        train_loss = []
+        for trainX, trainY in train_data:
+            Optimizer.zero_grad()
+            # print(trainX.shape,trainY.shape)
+            n = trainX.shape[0]
+            trainX = trainX.to(device)
+            # print(trainX.shape)
+            # trainX  = trainX.reshape(n, 28, 28)  # (batch,64,64)
+            trainX = trainX.squeeze()
+            # print(trainX.shape)
+            trainY = trainY.to(device)
+            # print(f'trainX.shape = {trainX.shape}')
+            pred_train = model(trainX)
+            # print('pred ==>',pred_train.shape,trainY.shape)
+            loss = loss_fc(pred_train, trainY)
+            loss.backward()
+            Optimizer.step()
+            train_loss.append(loss.item())
+            total_train_step += 1
+            if total_train_step % 100 == 0:
+                end_time = time.time()
+                print(f'Epoch:{epoch + 1}/{Epochs} , 训练次数:{total_train_step} '
+                      f', Loss:{loss.item():.4f}, 耗时: {(end_time - start_time):.1f} 秒')
+                      # f', Loss:{np.average(train_loss):.6f}, 耗时: {(end_time - start_time):.1f} 秒')
+
+        model.eval()
+        acc = 0
+        test_data_size = 0
+        test_loss = []
+        # 测试验证
+        with (torch.no_grad()):
+            for ii, (data, labels) in enumerate(test_data):  # 80
+                # print(data.shape,labels.shape)
+                n = data.shape[0]
+                data = data.to(device).reshape(n, 28, 28)
+                labels = labels.to(device)
+                pred_test = model(data)
+                loss_test = loss_fc(pred_test, labels)
+                test_loss.append(loss_test.item())
+                acc += (pred_test.argmax(1) == labels).sum().item()
+                test_data_size += labels.size(0)
+                writer.flush()
+        print(
+            f'acc={acc},test_data_size={test_data_size} | 测试集整体 ->{color.RED} Loss avg:{np.average(test_loss):.6f} , Acc :{(acc / test_data_size * 100):.3f}% {color.END}')
+        # writer.add_image('dont predict img',cant_img)
+        writer.add_scalar('test_loss_avg', np.average(test_loss), total_test_step)
+        writer.add_scalar('test_acc', acc / test_data_size, total_test_step)
+        total_test_step += 1
+        torch.save(model.state_dict(), f'../data/model/torch_rnn_module_{epoch + 1}.pth')
+        writer.flush()
+
+def train_face():
+    ############################# 训练数据参数 #########################
+    input_size = 64  # face: h = 64 , mnist: h= 28
+    hidden_size = 150
+    num_layers = 10
+    output_size = 40  # face: 40个人 , mnist : 10 个数字
+    total_train_step = 0  # 总体训练集次数
+    total_test_step = 0  # 总体验证集次数
+
+    ##
+    model = CustomTorchRNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, output_size=output_size)
+    print('model initialized')
+    Optimizer = torch.optim.SGD(model.parameters(), lr=Learning_Rate, momentum=Momentum)
+
     train_data, test_data = data_loader(BATCH_SIZE,device)
-    train_data_size = len(train_data)
-    test_data_size = len(test_data)
 
     loss_fc = nn.CrossEntropyLoss()
     start_time = time.time()
@@ -175,53 +245,56 @@ if __name__ == '__main__':
         model.train()
         train_loss = []
         for trainX , trainY in train_data:
-            optimizer.zero_grad()
+            Optimizer.zero_grad()
             # print(trainX,trainY.shape)
-            n = trainX.shape[0]
-            trainX = trainX.to(device).reshape(n,64,64)
+            # n = trainX.shape[0]
+            trainX = trainX.to(device) # (batch,64,64)
+            trainX = trainX.permute(0, 2, 1)
+            # print(trainX.shape)
             trainY = trainY.to(device)
-            pred_train = model(trainX).squeeze()
+            # print(f'trainX.shape = {trainX.shape}')
+            pred_train = model(trainX)
             # print('pred ==>',pred_train.shape,trainY.shape)
             loss = loss_fc(pred_train, trainY)
             loss.backward()
-            optimizer.step()
+            Optimizer.step()
             train_loss.append(loss.item())
             total_train_step += 1
-            if total_train_step % 10 == 0:
+            if total_train_step % 5 == 0:
                 end_time = time.time()
                 print(f'Epoch:{epoch + 1}/{Epochs} , 训练次数:{total_train_step} '
-                      f', Loss:{np.average(train_loss):.6f}, 耗时: {(end_time - start_time):.1f} 秒')
+                      f', Loss:{loss.item():.6f}, 耗时: {(end_time - start_time):.1f} 秒')
 
         model.eval()
         acc = 0
         test_data_size = 0
         test_loss = []
-        cant_img = []
         # 测试验证
         with (torch.no_grad()):
-            for ii, (data, labels, img) in enumerate(test_data):  # 80
-                # print(data.shape,labels.shape)
-                n = data.shape[0]
-                data = data.to(device).reshape(n,64,64)
+            for ii, (data, labels) in enumerate(test_data):  # 80
+                data = data.to(device)
                 labels = labels.to(device)
-                pred_test = model(data).squeeze()
+                pred_test = model(data)
                 loss_test = loss_fc(pred_test, labels)
                 test_loss.append(loss_test.item())
                 acc += (pred_test.argmax(1) == labels).sum().item()
                 test_data_size += labels.size(0)
-                # 获取为识别的照片
-                cant_img += img[labels != pred_test.argmax(1)]
-                # 下面这个取反的逻辑错误
-                ### cant_img +=img[~torch.isin(labels,pred_test.argmax(1))]
-            for index, imt in enumerate(cant_img):  # bach size 60
-                writer.add_image(f'Un_Pred_Img/epoch:{epoch + 1}/{index + 1}.img', imt,
-                                 dataformats='HW', global_step=index + 1)
-                writer.flush()
+
         print(f'acc={acc},test_data_size={test_data_size} | 测试集整体 ->{color.RED} Loss avg:{np.average(test_loss):.6f} , Acc :{(acc / test_data_size * 100):.3f}% {color.END}')
-        # writer.add_image('dont predict img',cant_img)
         writer.add_scalar('test_loss_avg', np.average(test_loss), total_test_step)
         writer.add_scalar('test_acc', acc / test_data_size, total_test_step)
         total_test_step += 1
         torch.save(model.state_dict(), f'../data/model/torch_rnn_module_{epoch + 1}.pth')
         writer.flush()
-writer.close()
+
+
+if __name__ == '__main__':
+
+    train_face()
+
+    # train_mnist()
+
+
+
+
+    writer.close()
