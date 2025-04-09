c2c4afe7d2bc3fdafd76d56c43513a7aae090efb
虫子 <wangxiyue.xy@163.com>
2025-04-09 10:41:54 +0800

    完成 homework1 homework2 下载数据

--- wangxiyue/week06/RNN.py
+++ wangxiyue/week06/RNN.py
@@ -2,9 +2,7 @@
 #1. 实验使用不同的RNN结构，实现一个人脸图像分类器。至少对比2种以上结构训练损失和准确率差异，如：LSTM、GRU、RNN、BiRNN等。要求使用tensorboard，提交代码及run目录和可视化截图。
 #   https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_olivetti_faces.html
 #
-#2. 使用RNN实现一个天气预测模型，能预测1天和连续5天的最高气温。要求使用tensorboard，提交代码及run目录和可视化截图。
-#   数据集：URL_ADDRESS   数据集：https://www.kaggle.com/datasets/smid80/weatherww2
-#
+
 import os
 import shutil
 import time
@@ -145,7 +143,7 @@ device = check_device()
 torch.set_default_device(device)
 ############################# 超参 #################################
 Learning_Rate = 0.001
-Epochs = 100
+Epochs = 50
 BATCH_SIZE = 16
 Momentum = 0.9
 #
---
+++ wangxiyue/week06/weather_rnn.py
@@ -0,0 +1,49 @@
+#2. 使用RNN实现一个天气预测模型，能预测1天和连续5天的最高气温。要求使用tensorboard，提交代码及run目录和可视化截图。
+#   数据集：URL_ADDRESS   数据集：https://www.kaggle.com/datasets/smid80/weatherww2
+#
+import csv
+
+from torch import nn
+
+
+#
+#STA、 Date, Precip ,WindGustSpd,MaxTemp,MinTemp,MeanTemp,Snowfall,PoorWeather,YR,MO,DA,PRCP,DR,SPD,MAX,MIN,MEA,SNF,SND,FT,FB,FTI,ITH,PGT,TSHDSBRSGF,SD3,RHX,RHN,RVG,WTE
+#站号、日期、  降水量。  最大风 ， 最大温度，最小温度，平均温度 ， 降雪，
+def load_weather_data(path):
+    res  = {}
+    data = []
+    with open(path,'r' , encoding = 'utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            res.setdefault(row['STA'],{})[row['Date']] = float(row['MaxTemp'])
+    return res
+
+
+class RNNModel(nn.Module):
+
+    def __init__(self, input_size, hidden_size,num_layers, output_size , model_name):
+        drop = 0.3
+
+        super(RNNModel, self).__init__()
+        if model_name == 'LSTM':
+            self.rnn = nn.LSTM(input_size = input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True , dropout=drop)
+            self.fc = nn.Linear(hidden_size, output_size)
+        elif model_name == 'GRU':
+            self.rnn = nn.GRU(input_size = input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True ,dropout=drop)
+            self.fc = nn.Linear(hidden_size, output_size)
+        else : # 'RNN'
+            self.rnn = nn.RNN( input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True ,dropout=drop)
+            self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        output,end = self.rnn(x)
+        y = self.fc(output[:, -1, :])
+        # y = self.fc(end[-1,:,:])
+        return y
+
+if __name__ == '__main__':
+    # b t feature
+    res = load_weather_data('../data/SummaryofWeather.csv')
+    print(f'station num = {len(res)}') # 159个站点
+    print(f'time len = {len(res[list(res.keys())[0]])} ')
+
